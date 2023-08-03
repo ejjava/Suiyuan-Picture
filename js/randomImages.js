ae3861209dd450ba26067c0825f51a33cf64e337
root <root@localhost>
2023-08-04 00:32:22 +0800

    2023-08-04 00:32:21

---
+++ js/randomImages.js
@@ -0,0 +1,8 @@
+function getRandomImageUrls() {
+    return [
+       
+        "https://cdn.seovx.com/img/mom2018 (436).jpg",
+        "https://cdn.seovx.com/d//img/momcn-2D (59).jpg",
+        // 添加更多的图片URL
+    ];
+}
