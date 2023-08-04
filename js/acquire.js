aeb07fcafc70a09eb7a03b41c3053568aafbdc1f
root <root@localhost>
2023-08-04 21:42:41 +0800

    2023-08-04 21:42:40

---
+++ js/acquire.js
@@ -0,0 +1,13 @@
+<script>
+    var imageUrls = getRandomImageUrls(); // 从 randomImages.js 获取随机图片URL数组
+    var imgElements = document.getElementsByClassName("random-image");
+
+    function getRandomImageUrl() {
+        var randomIndex = Math.floor(Math.random() * imageUrls.length);
+        return imageUrls[randomIndex];
+    }
+
+    for (var i = 0; i < imgElements.length; i++) {
+        imgElements[i].src = getRandomImageUrl();
+    }
+</script>
