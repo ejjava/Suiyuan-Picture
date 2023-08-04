305342fcb41e2710bf0b94f165741dadf9ae150b
root <root@localhost>
2023-08-04 12:55:52 +0800

    2023-08-04 12:55:51

--- js/randomImages.js
+++ js/randomImages.js
@@ -1,8 +1,22 @@
 function getRandomImageUrls() {
     return [
-       
-        "https://cdn.seovx.com/img/mom2018 (436).jpg",
-        "https://cdn.seovx.com/d//img/momcn-2D (59).jpg",
+
+        "https://pic.bibiu.cc/2023/08/04/64cc7fc16ca01.jpg",
+        "https://pic.bibiu.cc/2023/08/04/64cc7ff498f01.jpg",
+        "https://pic.bibiu.cc/2023/08/04/64cc8018438ae.jpg",
+        "https://pic.bibiu.cc/2023/08/04/64cc8058208de.jpg",
+        "https://pic.bibiu.cc/2023/08/04/64cc8084ce759.jpg",
+        "https://pic.bibiu.cc/2023/08/04/64cc808c00a27.jpg",
+        "https://pic.bibiu.cc/2023/08/04/64cc808dbb112.jpg",
+        "https://pic.bibiu.cc/2023/08/04/64cc808fd56db.jpg",
+        "https://pic.bibiu.cc/2023/08/04/64cc809347c49.jpg",
+        "https://pic.bibiu.cc/2023/08/04/64cc80d1d6628.jpg",
+        "https://pic.bibiu.cc/2023/08/04/64cc80d228840.jpg",
+        "https://pic.bibiu.cc/2023/08/04/64cc80d2af380.jpg",
+        "https://pic.bibiu.cc/2023/08/04/64cc80d80f13e.jpg",
+        "https://pic.bibiu.cc/2023/08/04/64cc80d80592f.jpg",
+        "https://pic.bibiu.cc/2023/08/04/64cc80d81786d.png",
+
         // 添加更多的图片URL
     ];
 }
